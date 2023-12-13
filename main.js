c4ece2b57409dfe8524d6d8cc879916bba17ee9b
Tanner Rogers <trroger2@alumni.ncsu.edu>
2023-12-12 21:25:44 -0500

    finished interactive map part 2

--- main.js
+++ main.js
@@ -4,3 +4,18 @@
 //3 - User should be able to select a business type from a list and map 5 nearest locations on the map using Foursquare API
 
 //Get User's Current Location
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
+
+console.log(getCoords())
+
+//Select Business Type
+function saveSelection(){
+    selectBusiness = document.getElementById('businesses');
+    output = selectBusiness.value;
+    alert(output)
+}
