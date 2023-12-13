6cf714ea8f0b7f54655636661a1775eed2bcacfe
Tanner Rogers <trroger2@alumni.ncsu.edu>
2023-12-12 19:12:54 -0500

    first set up

---
+++ main.js
@@ -0,0 +1,6 @@
+//Main Goals: 
+//1 - Obtain User's Current Location
+//2 - Map the Location on Leaflet
+//3 - User should be able to select a business type from a list and map 5 nearest locations on the map using Foursquare API
+
+//Get User's Current Location
