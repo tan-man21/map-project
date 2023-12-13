9ecd3be709baa61f6aa8fd42c4cf3aeb1d9d35d6
Tanner Rogers <trroger2@alumni.ncsu.edu>
2023-12-13 16:09:49 -0500

    Data can be retreived when clicking save button

--- main.js
+++ main.js
@@ -15,7 +15,114 @@ async function getCoords(){
 function saveSelection(){
     selectBusiness = document.getElementById('businesses');
     output = selectBusiness.value;
-    alert(output)
+    if(output === 'restaurant'){
+        console.log(getRestaurants())
+    }else if(output === 'coffee'){
+        console.log(getCoffee())
+    }else if(output === 'hotel'){
+        console.log(getHotels)
+    }else if(output === 'market'){
+        console.log(getMarkets)
+    }
 }
+
+// Fetching Local Restaurants
+async function getRestaurants (){
+    let coordinates = await getCoords()
+    const options = await {
+        method: 'GET',
+        headers: {
+        accept: 'application/json',
+        Authorization: 'fsq3mWIUw5lI9kr5aXW1Ta/ESQSIq7sPBWWIewtV2jLNPF4='
+        }
+    };
+    
+    await fetch(`https://api.foursquare.com/v3/places/search?query=restaurants&ll=${coordinates}&radius=5000&limit=5`, options)
+        .then(response => response.json())
+        .then(data => {
+            const mappedData = data.results.map(venue => ({
+                name: venue.name,
+                location: venue.location,
+            }));
+            console.log(mappedData);
+            return mappedData;
+        })
+        .catch(err => console.error(err));
+};
+
+//Fetching Local Coffee Shops
+async function getCoffee (){
+    let coordinates = await getCoords()
+    const options = await {
+        method: 'GET',
+        headers: {
+        accept: 'application/json',
+        Authorization: 'fsq3mWIUw5lI9kr5aXW1Ta/ESQSIq7sPBWWIewtV2jLNPF4='
+        }
+    };
+    
+    await fetch(`https://api.foursquare.com/v3/places/search?query=coffee&ll=&${coordinates}radius=5000&limit=5`, options)
+        .then(response => response.json())
+        .then(data => {
+            const mappedData = data.results.map(venue => ({
+                name: venue.name,
+                location: venue.location,
+            }))
+            console.log(mappedData);
+            return mappedData;
+        })
+        .catch(err => console.error(err));
+};
+
+
+//Fetch Local Hotels
+async function getHotels (){
+    let coordinates = await getCoords()
+    const options = await {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'fsq3mWIUw5lI9kr5aXW1Ta/ESQSIq7sPBWWIewtV2jLNPF4='
+        }
+      };
+      
+      await fetch(`https://api.foursquare.com/v3/places/search?query=hotels&ll=${coordinates}&radius=5000&limit=5`, options)
+        .then(response => response.json())
+        .then(data => {
+            const mappedData = data.results.map(venue => ({
+                name: venue.name,
+                location: venue.location,
+            }))
+            console.log(mappedData);
+            return mappedData;
+        })
+        .catch(err => console.error(err));
+};
+
+
+//Fetch Local Markets
+async function getMarkets(){
+    let coordinates = await getCoords()
+    const options = await {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'fsq3mWIUw5lI9kr5aXW1Ta/ESQSIq7sPBWWIewtV2jLNPF4='
+        }
+      };
+      
+      await fetch(`https://api.foursquare.com/v3/places/search?query=market&ll=${coordinates}&radius=5000&limit=5`, options)
+        .then(response => response.json())
+        .then(data => {
+            const mappedData = data.results.map(venue => ({
+                name: venue.name,
+                location: venue.location,
+            }))
+            console.log(mappedData);
+            return mappedData;
+        })
+        .catch(err => console.error(err));
+};
+
 
 //Creating Array of Business Objects
